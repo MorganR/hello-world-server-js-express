15e78506e1a7b89c37c4f17600bbb41c669ca7d0
Morgan Roff <roffmorgan@gmail.com>
2022-10-23 17:52:20 +0200

    Add static file support

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 "use strict";
 const express = require("express");
+const mime = require("mime");
+const fs = require("fs/promises");
+const path = require("path");
 
 function getPort() {
   const fromEnv = Number.parseInt(process.env.PORT);
@@ -14,10 +17,45 @@ function helloHandler(req, res) {
   res.send("Hello, world!");
 }
 
+async function usePrecompressedIfPresent(req, res, next) {
+  if (req.url.includes("..")) {
+    return res.status(404).end();
+  }
+
+  const ext = path.extname(req.url);
+  const resType = mime.getType(ext);
+  if (
+    req.acceptsEncodings("br") &&
+    (resType.startsWith("text/") ||
+      resType.startsWith("application/") ||
+      resType == "image/svg+xml")
+  ) {
+    const compressedLocalPath = "./static" + req.url + ".br";
+    try {
+      const compressed = await fs.readFile(compressedLocalPath);
+      res.set("Content-Encoding", "br");
+      res.type(ext);
+      return res.send(compressed);
+    } catch {
+      // Ignore the error, this likely means the precompressed file didn't exist.
+    }
+  }
+
+  next();
+}
+
 const app = express();
 const port = getPort();
 
 app.get("/hello", helloHandler);
+app.use(
+  "/static",
+  usePrecompressedIfPresent,
+  express.static("static", {
+    index: false,
+    redirect: false,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
