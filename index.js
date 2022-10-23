7ce6b0cff3ae61f93050e886ef8f1f69374278d5
Morgan Roff <roffmorgan@gmail.com>
2022-10-23 20:15:18 +0200

    Enable /hello Brotli compression

--- index.js
+++ index.js
@@ -3,17 +3,53 @@ const express = require("express");
 const mime = require("mime");
 const fs = require("fs/promises");
 const path = require("path");
+const zlib = require("zlib");
+const {
+  constants: { BROTLI_PARAM_MODE, BROTLI_MODE_TEXT },
+} = require("zlib");
 
 function getPort() {
   const fromEnv = Number.parseInt(process.env.PORT);
   return Number.isNaN(fromEnv) ? 8080 : fromEnv;
 }
 
+const MAX_NAME_LENGTH = 500;
+
 function helloHandler(req, res) {
+  res.type("text/plain");
+
   if (req.query.name?.length > 0) {
-    res.send(`Hello, ${req.query.name}!`);
+    if (req.query.name.length > MAX_NAME_LENGTH) {
+      return res.status(400).end();
+    }
+    const result = `Hello, ${req.query.name}!`;
+
+    // Compress the response if it's long and supported.
+    if (req.acceptsEncodings("br") && result.length > 256) {
+      zlib.brotliCompress(
+        result,
+        {
+          params: {
+            [BROTLI_PARAM_MODE]: BROTLI_MODE_TEXT,
+          },
+        },
+        (err, compressedResult) => {
+          if (err !== null) {
+            console.error(`Failed to compress: ${err}`);
+            res.send(result);
+          } else {
+            res.set("Content-Encoding", "br");
+            res.send(compressedResult);
+          }
+        }
+      );
+      return;
+    }
+
+    res.send(result);
     return;
   }
+
   res.send("Hello, world!");
 }
 
