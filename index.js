54f0c6a41d75e5da3fd709fca7f59e3ce3bd887a
Morgan Roff <roffmorgan@gmail.com>
2022-10-23 17:05:18 +0200

    Implement /hello handler

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 "use strict";
+const express = require("express");
 
 function getPort() {
   const fromEnv = Number.parseInt(process.env.PORT);
   return Number.isNaN(fromEnv) ? 8080 : fromEnv;
 }
 
-const express = require("express");
+function helloHandler(req, res) {
+  if (req.query.name?.length > 0) {
+    res.send(`Hello, ${req.query.name}!`);
+    return;
+  }
+  res.send("Hello, world!");
+}
+
 const app = express();
 const port = getPort();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get("/hello", helloHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
